1a6d4757399c6d1c5a21613a75e14d478f8eb1cb
kejun <ckj119940887@gmail.com>
2026-07-12 19:08:11 -0400

    02-EXPR-COMPILER

---
+++ frontend/02-EXPR-COMPILER/main.cc
@@ -0,0 +1,34 @@
+#include "lexer.h"
+#include "llvm/Support/MemoryBuffer.h"
+#include "llvm/Support/ErrorOr.h"
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        printf("input your filename\n");
+        return 0;
+    }
+
+    const char *fileName = argv[1];
+    static llvm::ErrorOr<std::unique_ptr<llvm::MemoryBuffer>> buf = llvm::MemoryBuffer::getFile(fileName);
+    if (!buf)
+    {
+        llvm::errs() << "can't open file !!!\n";
+        return -1;
+    }
+
+    std::unique_ptr<llvm::MemoryBuffer> memBuf = std::move(*buf);
+    Lexer lex(memBuf->getBuffer());
+
+    Token tok;
+    while (true)
+    {
+        lex.NextToken(tok);
+        if (tok.tokenType != TokenType::eof)
+            tok.Dump();
+        else
+            break;
+    }
+
+    return 0;
+}
